4e3f1e789f5f409b0bf6435fc213815b021705d4
Victor Ezaki <victorezaki19@gmail.com>
2024-10-20 10:59:38 -0300

    feat: page home finalizada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import './styles.css'
 import rick from './../../assets/rick.png'
 import morty from './../../assets/image.png'
+import justin from './../../assets/Justin.jpg'
+import james from './../../assets/james.webp'
 
 export default function Home(){
     return(
@@ -14,11 +16,26 @@ export default function Home(){
                         </div>
                         <div id="button-watch">
                             <img src={ morty } alt="" id='morty' />
-                            <a href="#section-watch">Onde assistir</a>
+                            <a href="#section-create">Criadores</a>
                         </div>
                     </div>
                     <img src={ rick } alt="" id='rick' />
                 </div>
+
+                <section id='section-create'>
+                    <div id="container">
+                        <div className="create">
+                            <img src={ justin } alt="" id='justin' />
+                            <h1 className='title-create'>Justin Roiland</h1>
+                            <p className='text-create'>Mark Justin Roiland é um ator, dublador, animador, escritor, produtor, diretor, e comediante americano.</p>
+                        </div>
+                        <div className="create">
+                            <img src={ james } alt="" />
+                            <h1 className='title-create'>Dan Harmon</h1>
+                            <p className='text-create'>Daniel James Harmon é um roteirista e produtor americano.</p>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     )
