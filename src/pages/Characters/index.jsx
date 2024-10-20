90175739665c031674e5cf5bca1f939cb9c3f69d
Victor Ezaki <victorezaki19@gmail.com>
2024-10-20 14:17:50 -0300

    feat: pagination concluida

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react'
 import './styles.css'
 import CardCharacter from '../../components/CardCharacters';
+import Pagination from '../../components/Pagination';
 
 export default function Characters() {
     const [conteudo, setConteudo] = useState(<>Carregando...</>);
+    const [busca, setBusca] = useState('');
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
 
     async function getCharacter() {
         const reqOptions = {
@@ -12,7 +16,7 @@ export default function Characters() {
         }
 
         const response = await fetch(
-            "https://rickandmortyapi.com/api/character",
+            `https://rickandmortyapi.com/api/character?page=${page}${busca}`,
             reqOptions
         )
 
@@ -20,14 +24,16 @@ export default function Characters() {
             throw new Error("HTTP Error!");
         }
 
-        const apiResponse = await response.json();
+        const data = await response.json();
 
-        return apiResponse;
+        return { info: data.info, char: data.results }
     }
 
     async function buildCard() {
-        const consult = await getCharacter();
-        return consult.results.map(character => <CardCharacter data={character} />);
+        const { char: characters, info } = await getCharacter()
+        setTotalPages(info.pages)
+
+        return characters.map(character => <CardCharacter data={character} />);
     }
 
     useEffect(() => {
@@ -36,12 +42,18 @@ export default function Characters() {
         }
 
         getConteudo()
-    }, [])
+    }, [page, busca])
 
 
     return (
         <>
             <main id="main-characters">
+                <Pagination
+                    page={page}
+                    totalPages={totalPages}
+                    onPageChange={setPage}
+                />
+                
                 <div id='api-content'>
                     {conteudo}
                 </div>
