c97c3a434090efa6575f42fd1fb5a36c593834c0
Victor Ezaki <victorezaki19@gmail.com>
2024-10-26 16:31:43 -0300

    feat: Como fazer um mock para uma api

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Characters from './pages/Characters';
+import Episodes from './pages/Episodes';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/personagens' element={<Characters />} />
+        <Route path='/episodios' element={<Episodes />} />
       </Routes>
       <Footer />
     </>
---
+++ src/components/CardEpisodes/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css'
+import capa from './../../assets/capa-episodes.jpg'
+
+export default function CardEpisodes({ data: episode }) {
+    return (
+        <>
+            <div id="card-episode">
+                <img src={capa} alt="" />
+                <h1 id='name-episode' className='card-infos'>
+                    {episode.name}
+                </h1>
+                <h2 className='card-infos'>Data: <span>{ episode.air_date }</span></h2>
+                <h2 className='card-infos'>Episódio: <span>{ episode.episode }</span></h2>
+            </div>
+        </>
+    )
+}
