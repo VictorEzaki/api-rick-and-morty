e0a6ec3f0aa46ad084f865dc398f2296b4cca52d
Victor Ezaki <victorezaki19@gmail.com>
2024-10-26 17:38:47 -0300

    feat: fetch e filtros finalizados, falta apenas fazer o carrosel e selecionar uma imagem para cada temporada

--- src/components/FilterEpisode/index.jsx
+++ src/components/FilterEpisode/index.jsx
@@ -1,8 +1,32 @@
 import './styles.css'
 
-export default function FilterEpisode() {
+export default function FilterEpisode({ busca, setBusca }) {
+    function buildFilterEP(type, value){
+        const filters = new URLSearchParams(busca);
+        const filter = filters.get(type);
+
+        if (filter === value) {
+            filters.delete(type);
+        } else {
+            filters.set(type, value);
+        }
+
+        setBusca(filters.toString())
+    }
+
+
     return(
         <>
+            <div id='filter-episode'>
+                <select id="filter-season" name="season" onChange={(event) => buildFilterEP('episode', event.target.value)}>
+                    <option disabled>Temporadas</option>
+                    <option value="S01">Temporada 1</option>
+                    <option value="S02">Temporada 2</option>
+                    <option value="S03">Temporada 3</option>
+                    <option value="S04">Temporada 4</option>
+                    <option value="S05">Temporada 5</option>
+                </select>
+            </div>
         </>
     )
 }
--- src/pages/Episodes/index.jsx
+++ src/pages/Episodes/index.jsx
@@ -3,95 +3,52 @@ import './styles.css'
 import CardEpisodes from '../../components/CardEpisodes'
 import FilterEpisode from '../../components/FilterEpisode'
 
-const mock = {
-    results: [
-        {
-            "id": 1,
-            "name": "Pilot",
-            "air_date": "December 2, 2013",
-            "episode": "S01E01",
-            "characters": [
-                "https://rickandmortyapi.com/api/character/1",
-                "https://rickandmortyapi.com/api/character/2",
-                "https://rickandmortyapi.com/api/character/35",
-                "https://rickandmortyapi.com/api/character/38",
-                "https://rickandmortyapi.com/api/character/62",
-                "https://rickandmortyapi.com/api/character/92",
-                "https://rickandmortyapi.com/api/character/127",
-                "https://rickandmortyapi.com/api/character/144",
-                "https://rickandmortyapi.com/api/character/158",
-                "https://rickandmortyapi.com/api/character/175",
-                "https://rickandmortyapi.com/api/character/179",
-                "https://rickandmortyapi.com/api/character/181",
-                "https://rickandmortyapi.com/api/character/239",
-                "https://rickandmortyapi.com/api/character/249",
-                "https://rickandmortyapi.com/api/character/271",
-                "https://rickandmortyapi.com/api/character/338",
-                "https://rickandmortyapi.com/api/character/394",
-                "https://rickandmortyapi.com/api/character/395",
-                "https://rickandmortyapi.com/api/character/435"
-            ],
-            "url": "https://rickandmortyapi.com/api/episode/1",
-            "created": "2017-11-10T12:56:33.798Z"
-        },
-        {
-            "id": 2,
-            "name": "Lawnmower Dog",
-            "air_date": "December 9, 2013",
-            "episode": "S01E02",
-            "characters": [
-                "https://rickandmortyapi.com/api/character/1",
-                "https://rickandmortyapi.com/api/character/2",
-                "https://rickandmortyapi.com/api/character/38",
-                "https://rickandmortyapi.com/api/character/46",
-                "https://rickandmortyapi.com/api/character/63",
-                "https://rickandmortyapi.com/api/character/80",
-                "https://rickandmortyapi.com/api/character/175",
-                "https://rickandmortyapi.com/api/character/221",
-                "https://rickandmortyapi.com/api/character/239",
-                "https://rickandmortyapi.com/api/character/246",
-                "https://rickandmortyapi.com/api/character/304",
-                "https://rickandmortyapi.com/api/character/305",
-                "https://rickandmortyapi.com/api/character/306",
-                "https://rickandmortyapi.com/api/character/329",
-                "https://rickandmortyapi.com/api/character/338",
-                "https://rickandmortyapi.com/api/character/396",
-                "https://rickandmortyapi.com/api/character/397",
-                "https://rickandmortyapi.com/api/character/398",
-                "https://rickandmortyapi.com/api/character/405"
-            ],
-            "url": "https://rickandmortyapi.com/api/episode/2",
-            "created": "2017-11-10T12:56:33.916Z"
-        }
-    ]
-}
-
 export default function Episodes() {
     const [conteudo, setConteudo] = useState(<>Carregando...</>)
+    const [busca, setBusca] = useState('');
 
-    function getEpisodes() {
-        return mock
+    async function getEpisodes() {
+        const reqOptions = {
+            method: "GET",
+            redirect: "follow"
+        }
+
+        const response = await fetch(
+            `https://rickandmortyapi.com/api/episode?${busca}`,
+            reqOptions
+        )
+
+        if (!response.ok) {
+            throw new Error("HTTP Error!");
+        }
+
+        const data = await response.json();
+
+        return { epi: data.results }
     }
 
-    function buildEpisode() {
-        const episodes = getEpisodes()
+    async function buildEpisode() {
+        const { epi: episodes } = await getEpisodes()
 
-        return episodes.results.map(episode => <CardEpisodes data={episode} />)
+        return episodes.map(episode => <CardEpisodes data={episode} />)
     }
 
     useEffect(() => {
-        function getConteudo() {
-            setConteudo(buildEpisode())
+        async function getConteudo() {
+            setConteudo(await buildEpisode())
         }
 
         getConteudo()
-    }, [])
+    }, [busca])
 
 
     return (
         <>
             <main id='main-episodes'>
-                <FilterEpisode />
+                <FilterEpisode
+                    busca={busca}
+                    setBusca={setBusca}
+                />
                 <div id="episodes">
                     {conteudo}
                 </div>
